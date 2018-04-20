145aadf518b28da75a9c85abb6f6fda816265335
Abhishek Khandal <akhandal69@gmail.com>
2018-04-20 20:10:14 +0530

    Docker container with gotty

--- docker.py
+++ docker.py
@@ -10,7 +10,7 @@ fs = cgi.FieldStorage()
 lang = "CPP"
 
 # Filename: Random number between 1 and 10^19
-code_file = str(random.randint(1,10**19))
+filename = str(random.randint(1,10**19))
 
 def Docker_RMI():
 	# Clear the clutter, delete old Docker Images
@@ -21,16 +21,17 @@ def Docker_RMI():
 			docker_image=build_images.split('\n')[i].split()[2]
 
 			# Preserve important images used for building new images
-			if(docker_image not in ['8ac48589692a','efb6baa1169f','8357b3fcbe41','1b3de68a7ff8']):
+			if(docker_image not in ['8357b3fcbe41', '8ac48589692a', 'efb6baa1169f', '8357b3fcbe41', '1b3de68a7ff8']):
 				subprocess.getoutput("docker rmi -f {image}".format(image = docker_image))
 	except:
 		print ("")		
 
 def Lang_C_CPP(lang):
-		cpp_file = code_file + ".{lang}".format(lang = lang)
-		cpp_executable = cpp_file.split('.')[0] + ".o"
+		ext = "c" if lang == "C" else "cpp" 
+		code_file = filename + ".{ext}".format(ext = ext)
+		executable_file = filename + ".o"
 		
-		CPP_Program = """
+		program_code = """
 		#include <iostream>
 		using namespace std;
 
@@ -39,29 +40,38 @@ def Lang_C_CPP(lang):
 		return 0;
 		}
 		"""
-		write_cpp_file = open("cpp_files/" + cpp_file, "w")
-		write_cpp_file.write(CPP_Program)
-		write_cpp_file.close()
+		write_code_file = open("code_files/" + code_file, "w")
+		write_code_file.write(program_code)
+		write_code_file.close()
 
 		
-		write_dockerfile = open("cpp_files/"+ code_file, "w")
+		#write_dockerfile = open("code_files/"+ code_file, "w")
 
 		# Absolute path needed for COPY as Docker executes a cache file from a tmp dir 
 		Dockerfile = """
 	FROM gcc:4.9
-	COPY {cpp_file} /usr/src/mycpp/
-	WORKDIR /usr/src/mycpp
-	RUN {compile} -o {cpp_executable} {cpp_file}
-	CMD ["./{cpp_executable}"]
-		""".format(cpp_file = cpp_file, cpp_executable = cpp_executable, compile = "gcc" if lang == "C" else "g++")
+	COPY {code_file} /usr/src/myprogram/
+	WORKDIR /usr/src/myprogram
+	RUN {compile} -o {executable_file} {code_file}
+	CMD ["./{executable_file}"]
+		""".format(code_file = code_file, executable_file = executable_file, compile = "gcc" if lang == "C" else "g++")
 
-		# Write the following Dockerfile contents to the location and file: [ "cpp_files" + PROGRAM/File ]
-		write_dockerfile.write(Dockerfile)
-		write_dockerfile.close()
-
-		call(["cd cpp_files && docker build -t {label} -f {label} .".format(label = code_file)],shell=True)
+		# Write the following Dockerfile contents to the location and file: [ "code_files" + PROGRAM/File ]
+		#write_dockerfile.write(Dockerfile)
+		#write_dockerfile.close()
+		# "sudo docker cp UTK28.CPP c1ce27253d7e:/main.cpp"
+		compiler = "gcc" if lang == "C" else "g++"
 		
-		call(["gotty -w --term PaaS docker run -it --rm {label}:latest ".format(label = code_file)],shell=True)
+		# Container ID to be smaller in digits and hash to be always positive
+		container_id = abs(hash(filename))
+
+		call(["docker run -itd --rm --name {label} gcc".format(label = container_id)],stdout=None,shell=True)
+		call(["cd code_files && docker cp {codefile} {container}:/{codefile}".format(codefile = code_file, container = container_id)],stdout=None,shell=True)
+		call(["gotty -w --once docker attach {container}".format(container = container_id)],stdout=None,shell=True)
+		
+
+		#call(["cd code_files && docker build -t {label} -f {label} .".format(label = code_file)],shell=True)
+		#call(["gotty -w --term PaaS docker run -it --rm {label}:latest ".format(label = code_file)],shell=True)
 
 def Lang_Python(ver):
 		# To use Python2 or 3 Docker container
@@ -90,7 +100,7 @@ def choice():
 
 def main():
 	Docker_RMI()
-	Process()
+	Lang_C_CPP(lang)
 
 main()
 
