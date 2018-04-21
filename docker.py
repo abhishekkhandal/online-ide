e61e1953d5bdc8767ce239ad58362bf64e49eef9
Abhishek Khandal <akhandal69@gmail.com>
2018-04-21 17:18:23 +0530

    compile C/C++ code in docker {container}:/home/ + improvements

--- docker.py
+++ docker.py
@@ -3,7 +3,7 @@ import cgi
 import random
 global subprocess
 import subprocess
-from subprocess import call
+from subprocess import call, DEVNULL, STDOUT, check_call, Popen, PIPE
 
 fs = cgi.FieldStorage()
 
@@ -15,7 +15,11 @@ filename = str(random.randint(1,10**19))
 def Docker_RMI():
 	# Clear the clutter, delete old Docker Images
 	process = subprocess.Popen(['docker', 'images'], stdout=subprocess.PIPE)
-	build_images = process.communicate()	
+	build_images = process.communicate()
+
+	# Stop all stopped containers
+	#Popen(['docker', 'rm', '$(docker ps -a -q)'])
+
 	try:
 		for i in range(1,len(build_images.split("\n"))):
 			docker_image=build_images.split('\n')[i].split()[2]
@@ -32,14 +36,13 @@ def Lang_C_CPP(lang):
 		executable_file = filename + ".o"
 		
 		program_code = """
-		#include <iostream>
-		using namespace std;
+#include <iostream>
+using namespace std;
 
-		int main(){
-		cout<<"hello world!";
-		return 0;
-		}
-		"""
+int main(){
+cout<<"hello world!\\n";
+return 0;
+}"""
 		write_code_file = open("code_files/" + code_file, "w")
 		write_code_file.write(program_code)
 		write_code_file.close()
@@ -49,11 +52,12 @@ def Lang_C_CPP(lang):
 
 		# Absolute path needed for COPY as Docker executes a cache file from a tmp dir 
 		Dockerfile = """
-	FROM gcc:4.9
-	COPY {code_file} /usr/src/myprogram/
-	WORKDIR /usr/src/myprogram
-	RUN {compile} -o {executable_file} {code_file}
-	CMD ["./{executable_file}"]
+	FROM gcc:7.3
+	WORKDIR /home
+	RUN apt-get update && \
+    apt-get -y install gcc mono-mcs && \
+    rm -rf /var/lib/apt/lists/*
+	CMD ["./main"]
 		""".format(code_file = code_file, executable_file = executable_file, compile = "gcc" if lang == "C" else "g++")
 
 		# Write the following Dockerfile contents to the location and file: [ "code_files" + PROGRAM/File ]
@@ -65,10 +69,38 @@ def Lang_C_CPP(lang):
 		# Container ID to be smaller in digits and hash to be always positive
 		container_id = abs(hash(filename))
 
-		call(["docker run -itd --rm --name {label} gcc".format(label = container_id)],stdout=None,shell=True)
-		call(["cd code_files && docker cp {codefile} {container}:/{codefile}".format(codefile = code_file, container = container_id)],stdout=None,shell=True)
-		call(["gotty -w --once docker attach {container}".format(container = container_id)],stdout=None,shell=True)
+		# Generate random number within userable port range, check availability and use it
+		available = 0
+		while available == 0:
+			for i in range (1,200):
+				port = random.randint(1234,65535)
+				if PortCheck(port) == "available":
+					port = port
+					print (port)
+					break
+				break
+			break
+
+
+		#os.system("docker run -itd --rm --name {label} gcc".format(label = container_id))
+		#os.system("cd code_files && docker cp {codefile} {container}:/{codefile}".format(codefile = code_file, container = container_id))
+		#os.system("gotty -w --once docker attach {container}".format(container = container_id))
 		
+		#call(["docker run -itd --rm --name {label} gcc".format(label = container_id)],stdout=None,shell=True)
+		#call(["cd code_files && docker cp {codefile} {container}:/{codefile}".format(codefile = code_file, container = container_id)],stdout=None,shell=True)
+		#call(["gotty -w --once docker attach {container}".format(container = container_id)],stdout=None,shell=True)
+		
+
+		check_call(['docker', 'run', '-itd', '--rm', '--stop-timeout', '120','--name', '{label}'.format(label = container_id), '723924dec980'],stdout=DEVNULL,stderr=STDOUT)
+		check_call(['docker', 'cp', 'code_files/{codefile}'.format(codefile = code_file), '{container}:/home/main.{ext}'.format(codefile = code_file,container = container_id, ext = ext)],stdout=DEVNULL,stderr=STDOUT)
+		
+		# to execute command and make it display to user gotty screen
+		# docker exec -ti cfa856a1dda9 screen -S web -X stuff $'./main\n'
+		check_call(['docker', 'exec', '-ti', '{container}'.format(container = container_id), '{compiler}'.format(compiler = compiler), 'main.{ext}'.format(ext = ext), '-o', 'main'],stdout=DEVNULL,stderr=STDOUT)
+		call(['gotty', '-p', '{port}'.format(port = port), '-w', 'docker', 'attach', '{container}'.format(container = container_id)],stdout=DEVNULL,stderr=STDOUT)
+
+
+
 
 		#call(["cd code_files && docker build -t {label} -f {label} .".format(label = code_file)],shell=True)
 		#call(["gotty -w --term PaaS docker run -it --rm {label}:latest ".format(label = code_file)],shell=True)
@@ -87,10 +119,17 @@ def Lang_Python(ver):
 def ttyOverBrowser(port, docker_cmd):
 	subprocess.getoutput("ttyd -p {port} --once {cmd} &".format(port = port, cmd = docker_cmd))
 
+def PortCheck(port):
+	p1 = subprocess.Popen(["netstat", "-an"], stdout=PIPE)
+	p2 = subprocess.Popen(["grep", "{port}".format(port = port)], stdin=p1.stdout, stdout=PIPE)
+	p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+	output = p2.communicate()[0]
+	if (output):
+		return ("unavailable")
+	else:
+		return ("available")
+
 def choice():
-	# lang test
-	# lang = "C"
-	
 	if (lang == "C" or lang == "CPP"):
 		l = "c" if lang == "C" else "cpp"
 		Lang_C_CPP(l)
