f94767b1d854faaa1c8c0d60ed7a4c641bf89680
Abhishek Khandal <akhandal69@gmail.com>
2018-04-24 10:53:14 +0530

    'how' program in container to help non-linux users

--- docker.py
+++ docker.py
@@ -11,9 +11,9 @@ print ()
 
 fs = cgi.FieldStorage()
 
-#lang = str(fs.getvalue('language'))
+lang = str(fs.getvalue('language'))
 code = str(fs.getvalue('code'))
-lang = "CPP"
+#lang = "CPP"
 host = "http://localhost:"
 result = {}
 url = ""
@@ -30,7 +30,7 @@ def Docker_RMI():
 	#Popen(['docker', 'rm', '$(docker ps -a -q)'])
 
 	# TODO: kill containers running >= 1hr
-	# kill all running containers:
+	# kill all running containers and save the list in killed-containers.txt:
 	# docker ps | awk {' print $1 '} | tail -n+2 > tmp.txt; for line in $(cat killed-containers.txt); do docker kill $line; done;
 
 	try:
@@ -46,17 +46,39 @@ def Docker_RMI():
 def Lang_C_CPP(lang):
 		ext = "c" if lang == "c" else "cpp" 
 		code_file = filename + ".{ext}".format(ext = ext)
-		executable_file = filename + ".o"
+		executable_file =  "myprog"
 		
 		program_code = "{0}".format(code)
 		write_code_file = open("code_files/" + code_file, "w+")
 		if program_code == "None":
+			executable_file = "help"
 			program_code = """
 #include <iostream>
 using namespace std;
 
 int main(){
-cout<<"hello world!\\n";
+cout << "\033[1;31mForgot to type in some code,\033[0m \033[1;32m or was it intentional beta testing?\033[0m\\n";
+cout << "\033[1;36mHere\'s a hello world program for you!\033[0m\\n\\n";
+cout << "\\t\033[1;33m#include <iostream>\\n";
+cout << "\\tusing namespace std;\\n\\n";
+cout << "\\tint main() \\n";
+cout << "\\t{\\n";
+cout << "\\t    cout << \'Hello, World!\'\\n";
+cout << "\\t    return 0;\\n";
+cout << "\\t}\\n\033[0m";
+cout << endl << endl;
+cout << "\033[7;36mInstructions:\033[0m";
+cout << "\n\t Type \033[1;36mls\033[0m to list all the file available.";
+cout << "\n\t Use \033[1;36mNANO\033[0m or \033[1;36mVIM\033[0m to edit file.";
+cout << "\n\t Eg. \'\033[1;36mnano main.cpp\033[0m\' (without quotes).";
+cout << "\n\t\t\033[1;36mctrl + x\033[0m to write changes made in the file.\n\t\tPress \033[1;36my\033[0m to save the file.";
+cout << "\n\t Compile it manually: \'\033[1;36mg++ main.cpp -o myprog\033[0m\'.";
+cout << "\n\t Execute: \'\033[1;36m./myprog\033[0m\'.\n\n";
+cout << "\n\033[1;32m>> \033[0mCode from the textarea is saved in \033[1m\033[4;34mmain.cpp\033[0m\033[0m and compiled to \033[1m\033[4;34mmyprog\033[0m\033[0m";
+cout << "\n\033[1;32m>> \033[0mEdit the code in  terminal \033[1m\033[4;34mnano main.cpp\033[0m\033[0m";
+cout << "\n\033[1;32m>> \033[0mRun the compiled program: \033[1m\033[4;34m./myprog\033[0m\033[0m\n\n\n";
+cout << "\033[1;37mEnjoy!\n";
+cout << "-Abhishek\033[0m\n\n";
 return 0;
 }"""
 		write_code_file.write(program_code)
@@ -81,14 +103,17 @@ return 0;
 		# "docker build -t gcc-nano -f GCCDockerfile ." to custom build gcc:7.3 docker image with nano and vim 
 		launch_container = "docker run -itd --rm --stop-timeout 120 --name {container} gcc-nano && \
 		docker cp code_files/{codefile} {container}:/home/main.{ext} && \
-		docker exec {container} {compiler} main.{ext} -o main".format(codefile = code_file,container = container_id, ext = ext, compiler = compiler)
+		docker exec {container} {compiler} main.{ext} -o {exec}".format(codefile = code_file,container = container_id, ext = ext, compiler = compiler, exec = executable_file)
 		web_shell = "ttyd -p {port} -r 2 -d 0 docker attach {container}".format(container = container_id, port = port)
 		
+		# Execute the commands specified in the string commands above	
 		subprocess.Popen(launch_container, shell=True, close_fds=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 		subprocess.Popen(web_shell, shell=True, close_fds=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 		
+		# The ttyd URL
 		url = host + str(port)
 		
+		# Return JSON back to index.py
 		result['url'] = url
 		result['success'] = True
 		print (json.dumps(result))		
@@ -102,7 +127,6 @@ def Lang_Python(ver):
 	CMD ["{PyVer} {File}"]
 	""".format(File = File, ver = ver, PyVer = "Python2" if ver == 2 else "Python3")	
 	print (Dockerfile)
-
 
 def ttyOverBrowser(port, docker_cmd):
 	subprocess.getoutput("ttyd -p {port} --once {cmd} &".format(port = port, cmd = docker_cmd))
