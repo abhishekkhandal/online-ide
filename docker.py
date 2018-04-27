6efd02f684ed79e536273e418cce619915b9124b
Abhishek Khandal <akhandal69@gmail.com>
2018-04-27 08:31:29 +0530

    Improvements

--- docker.py
+++ docker.py
@@ -32,7 +32,10 @@ def Docker_RMI():
 	# kill all running containers and save the list in killed-containers.txt:
 	# docker ps | awk {' print $1 '} | tail -n+2 > killed-containers.txt; for line in $(cat killed-containers.txt); do docker kill $line; done;
 	# Kill ttyd processes to release ports
-	# ps -e | grep ttyd | awk {' print $1 '} | tail -n+2 > ttyd.txt for line in $(cat ttyd.txt); do sudo kill $line; done;
+	# ps -e | grep ttyd | awk {' print $1 '} | tail -n+3 > ttyd.txt for line in $(cat ttyd.txt); do sudo kill $line; done;
+	# ps -e | grep ttyd | awk {'print $1'} | tail -n+2 > ttyd.txt 
+    # for PID in $(cat ttyd.txt); do ps -p $PID -o pid,args=ARGS; done;
+
 	try:
 		for i in range(1,len(build_images.split("\n"))):
 			docker_image=build_images.split('\n')[i].split()[2]
@@ -51,13 +54,12 @@ def Lang_C_CPP(lang):
 		program_code = "{0}".format(code)
 		write_code_file = open("code_files/" + code_file, "w+")
 		if program_code == "None":
-			executable_file = "help"
 			program_code = """
 #include <iostream>
 using namespace std;
 
 int main(){
-cout << "\033[1;31mForgot to type in some code,\033[0m \033[1;32m or was it intentional beta testing?\033[0m\\n";
+cout << "\033[1;31mForgot to type in some code,\033[0m \033[1;32m or is it intentional beta testing?\033[0m\\n";
 cout << "\033[1;36mHere\'s a hello world program for you!\033[0m\\n\\n";
 cout << "\\t\033[1;33m#include <iostream>\\n";
 cout << "\\tusing namespace std;\\n\\n";
@@ -68,15 +70,16 @@ cout << "\\t    return 0;\\n";
 cout << "\\t}\\n\033[0m";
 cout << endl << endl;
 cout << "\033[7;36mInstructions:\033[0m";
-cout << "\\n\\t Type \033[1;36mls\033[0m to list all the file available.";
-cout << "\\n\\t Use \033[1;36mNANO\033[0m or \033[1;36mVIM\033[0m to edit file.";
-cout << "\\n\\t Eg. \'\033[1;36mnano main.cpp\033[0m\' (without quotes).";
-cout << "\\n\\t\t\033[1;36mctrl + x\033[0m to write changes made in the file.\\n\\t\tPress \033[1;36my\033[0m to save the file.";
-cout << "\\n\\t Compile it manually: \'\033[1;36mg++ main.cpp -o myprog\033[0m\'.";
-cout << "\\n\\t Execute: \'\033[1;36m./myprog\033[0m\'.\\n\\n";
-cout << "\\n\033[1;32m>> \033[0mCode from the textarea is saved in \033[1m\033[4;34mmain.cpp\033[0m\033[0m and compiled to \033[1m\033[4;34mmyprog\033[0m\033[0m";
-cout << "\\n\033[1;32m>> \033[0mEdit the code in  terminal \033[1m\033[4;34mnano main.cpp\033[0m\033[0m";
-cout << "\\n\033[1;32m>> \033[0mRun the compiled program: \033[1m\033[4;34m./myprog\033[0m\033[0m\\n\\n\\n";
+cout << "\\n\\t\033[1;32m*\033[0m Type \033[1;36mls\033[0m to list all the file available.";
+cout << "\\n\\t\033[1;32m*\033[0m Use \033[1;36mnano/vim {filename}\033[0m to edit file.";
+cout << "\\n\\t\\tEg. \033[1;36mnano main.cpp\033[0m";
+cout << "\\n\\t\\\t  \033[1;36mctrl + x\033[0m to write changes made in the file.\\n\\t\\t  Press \033[1;36my\033[0m to save the file.";
+cout << "\\n\\t\033[1;32m*\033[0m Compile it manually: \'\033[1;36mg++ main.cpp -o myprog\033[0m\'.";
+cout << "\\n\\t\033[1;32m*\033[0m Execute: \'\033[1;36m./myprog\033[0m\'.\\n\\n";
+cout << "\\n\033[1;32m>> \033[0mCode from textarea is saved in \033[1m\033[1;30;47m main.cpp \033[0m\033[0m and compiled to \033[1m\033[1;30;47m myprog \033[0m\033[0m";
+cout << "\\n\033[1;32m>> \033[0mEdit the code in  terminal \033[1m\033[1;30;47m nano main.cpp \033[0m\033[0m";
+cout << "\\n\033[1;32m>> \033[0mRun the compiled program: \033[1m\033[1;30;47m ./myprog \033[0m\033[0m";
+cout << "\\n\033[1;32m>> \033[0mUse command \033[1m\033[1;30;47m howto \033[0m\033[0m to display these instructions again\\n\\n\\n";
 cout << "\033[1;37mEnjoy!";
 cout << "\\n-Abhishek\033[0m\\n\\n";
 return 0;
@@ -101,10 +104,10 @@ return 0;
 		container_id = abs(hash(filename))
 
 		# "docker build -t gcc-nano -f GCCDockerfile ." to custom build gcc:7.3 docker image with nano and vim 
-		launch_container = "docker run -itd --rm --stop-timeout 120 --name {container} gcc-nano && \
+		launch_container = "docker run -itd --rm --stop-timeout 120 --name {container} gcc-nano howto && \
 		docker cp code_files/{codefile} {container}:/home/main.{ext} && \
 		docker exec {container} {compiler} main.{ext} -o {exec}".format(codefile = code_file,container = container_id, ext = ext, compiler = compiler, exec = executable_file)
-		web_shell = "ttyd -p {port} -r 2 -m 1 -d 0 docker attach {container}".format(container = container_id, port = port)
+		web_shell = "ttyd -p {port} -r 2 -d 0 docker attach {container}".format(container = container_id, port = port)
 		
 		# Execute the commands specified in the string commands above	
 		subprocess.Popen(launch_container, shell=True, close_fds=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
